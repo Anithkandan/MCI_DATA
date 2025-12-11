84603edc4bc22bdc4ebb073f90eafb6eeb9fd969
Anitha Kandan <anitha.kandan760@gmail.com>
2025-12-11 16:04:49 +0000

    latest cod

--- macros/soft_delete_active_customers.sql
+++ macros/soft_delete_active_customers.sql
@@ -1,43 +1,47 @@
 {% macro soft_delete_active_customers() %}
 
--- Soft delete existing active customers that appear in new incremental data
-UPDATE {{ ref('dim_customer') }} AS dim
+UPDATE DIM_CUSTOMER dim
 SET 
     Active_Flag = 'N',
     Modified_At = CURRENT_TIMESTAMP()
 WHERE dim.Active_Flag = 'Y'
   AND (dim.customer_account_number, dim.customer_email) IN (
-
-    -- CSAT
-    SELECT LEFT("CUSTOMER ACCOUNT NUMBER",100), emailaddress
-    FROM {{ source('mci_data','CSAT') }}
-    WHERE TO_DATE(date) > (
-        SELECT MAX_CREATIONDATE 
-        FROM {{ ref('control_table') }} 
-        WHERE TABLE_NAME='CSAT'
-    )
-
-    UNION
-
-    -- IVR
-    SELECT LEFT("ACCOUNT",100), email
-    FROM {{ source('mci_data','IVR') }}
-    WHERE TO_DATE(date) > (
-        SELECT MAX_CREATIONDATE 
-        FROM {{ ref('control_table') }} 
-        WHERE TABLE_NAME='IVR'
-    )
-
-    UNION
-
-    -- SUBEASE
-    SELECT '', customer_email
-    FROM {{ source('mci_data','SUBEASE') }}
-    WHERE TO_DATE(date) > (
-        SELECT MAX_CREATIONDATE 
-        FROM {{ ref('control_table') }} 
-        WHERE TABLE_NAME='SUBEASE'
-    )
-);
-
+ 
+        -------------------------------------------------------
+        -- CSAT
+        -------------------------------------------------------
+        SELECT 
+            LEFT("CUSTOMER ACCOUNT NUMBER",100),
+            emailaddress
+        FROM KS_DEV.MCI_DATA.CSAT c
+        WHERE TO_TIMESTAMP(c.date) > (
+            SELECT MAX_CREATIONDATE FROM CONTROL_TABLE WHERE TABLE_NAME='CSAT'
+        )
+ 
+        UNION
+ 
+        -------------------------------------------------------
+        -- IVR
+        -------------------------------------------------------
+        SELECT 
+            LEFT("ACCOUNT",100),
+            email
+        FROM KS_DEV.MCI_DATA.IVR i
+        WHERE TO_TIMESTAMP(i.date) > (
+            SELECT MAX_CREATIONDATE FROM CONTROL_TABLE WHERE TABLE_NAME='IVR'
+        )
+ 
+        UNION
+ 
+        -------------------------------------------------------
+        -- SUBEASE
+        -------------------------------------------------------
+        SELECT 
+            '',
+            customer_email
+        FROM KS_DEV.MCI_DATA.SUBEASE s
+        WHERE TO_TIMESTAMP(s.date) > (
+            SELECT MAX_CREATIONDATE FROM CONTROL_TABLE WHERE TABLE_NAME='SUBEASE'
+        )
+   );
 {% endmacro %}
