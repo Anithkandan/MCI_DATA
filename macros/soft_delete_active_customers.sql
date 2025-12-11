8a83abed64885b839ab4fdb051350997f929f20b
Anitha Kandan <anitha.kandan760@gmail.com>
2025-12-11 14:37:16 +0000

    commite changes

---
+++ macros/soft_delete_active_customers.sql
@@ -0,0 +1,27 @@
+{% macro soft_delete_active_customers() %}
+
+UPDATE {{ ref('dim_customer') }} AS dim
+SET 
+    Active_Flag = 'N',
+    Modified_At = CURRENT_TIMESTAMP()
+WHERE dim.Active_Flag = 'Y'
+  AND (dim.customer_account_number, dim.customer_email) IN (
+
+    SELECT LEFT("CUSTOMER ACCOUNT NUMBER",100), emailaddress
+    FROM {{ source('mci_data', 'csat') }}
+    WHERE TO_DATE(date) > (SELECT MAX_CREATIONDATE FROM {{ ref('control_table') }} WHERE TABLE_NAME='CSAT')
+
+    UNION
+
+    SELECT LEFT("ACCOUNT",100), email
+    FROM {{ source('mci_data', 'ivr') }}
+    WHERE TO_DATE(date) > (SELECT MAX_CREATIONDATE FROM {{ ref('control_table') }} WHERE TABLE_NAME='IVR')
+
+    UNION
+
+    SELECT '', customer_email
+    FROM {{ source('mci_data', 'subease') }}
+    WHERE TO_DATE(date) > (SELECT MAX_CREATIONDATE FROM {{ ref('control_table') }} WHERE TABLE_NAME='SUBEASE')
+);
+
+{% endmacro %}
